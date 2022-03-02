af844929252cd3e681aae5a08622324edf0f87c0
Berhane Beyene <bbeyene@MacBook-Pro.local>
2022-03-02 01:54:08 -0600

    try

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res)=>{
-    res.json({mesg: 'this is a message from the backend'})
+    res.json({mesg: 'Message from the backend'})
 })
 
 app.listen(3000, ()=>console.log('server listining a prto 3000'))
