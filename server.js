6da13f56b390fe5b97d0ceb02866bf39bc2ed45b
Berhane Beyene <bbeyene@MacBook-Pro.local>
2022-03-02 02:13:59 -0600

    udated server

--- server.js
+++ server.js
@@ -3,8 +3,34 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res)=>{
+app.get('/api', (req, res)=>{
     res.json({mesg: 'Message from the backend'})
+})
+app.get('/products', (req, res)=>{
+    res.json({
+        "firstName": "John",
+        "lastName": "Smith",
+        "isAlive": true,
+        "age": 27,
+        "address": {
+          "streetAddress": "21 2nd Street",
+          "city": "New York",
+          "state": "NY",
+          "postalCode": "10021-3100"
+        },
+        "phoneNumbers": [
+          {
+            "type": "home",
+            "number": "212 555-1234"
+          },
+          {
+            "type": "office",
+            "number": "646 555-4567"
+          }
+        ],
+        "children": [],
+        "spouse": null
+      })
 })
 
 app.listen(3000, ()=>console.log('server listining a prto 3000'))
