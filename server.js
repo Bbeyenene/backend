cc1ec53d14dbf444de7fac62012ca70751ee6352
Berhane Beyene <bbeyene@MacBook-Pro.local>
2022-03-02 01:39:16 -0600

    m

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.get('/api', (req, res)=>{
+app.get('/', (req, res)=>{
     res.json({mesg: 'this is a message from the backend'})
 })
 
